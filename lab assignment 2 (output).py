e78fc5a8b072e5455fb73abf8a3751578fd1e4bf
rajc953 <103478704+rajc953@users.noreply.github.com>
2022-05-13 13:43:48 +0530

    Add files via upload

---
+++ lab assignment 2 (output).py
@@ -0,0 +1,24 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/HP/Dropbox/PC/Desktop/assignment1/output of lab assigment 2.py
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Hey, Tushar yadavendu Here!
+ My SID is 21102027 
+ I am from Civil department and my CGPA is 9.9
+Hey, Tushar yadavendu Here!
+My SID is 21102027 
+I am from Civil department and my CGPA is 9.9
+8 58 50 224 40 14 0
+enter any sentence: hello my name is tushar
+Yes
+4
+enter the side of triangle: 23
+enter the side of triangle:34
+enter the side of triangle: 27
+yes
